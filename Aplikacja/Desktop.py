efc46a8b45737bff59cf352778ffa414cc8eee19
MarcinKuszner <marcin.kuszner75@gmail.com>
2018-07-31 09:18:19 +0200

    123

--- Aplikacja/Desktop.py
+++ Aplikacja/Desktop.py
@@ -8,21 +8,23 @@ topFrame.pack()
 bottomframe = Frame(root)
 bottomframe.pack(side=BOTTOM)
 
-button1 = Button(topFrame, text="nowy folder", fg="Black")
-button2 = Button(topFrame, text="lewo" , fg= "Black")
-button3 = Button(topFrame, text="prawo" , fg="Black")
-button4 = Button(topFrame, text="zdjęcie" , fg="Black")
-button5 = Button(topFrame, text="wycinanie punktowe", fg="Black")
-button6 = Button(topFrame, text="wycinanie większych obszarów", fg="Black")
+
+button1 = Button(root, text="nowy folder", fg="Black")
+button2 = Button(root, text="lewo" , fg= "Black")
+button3 = Button(root, text="prawo" , fg="Black")
+button4 = Button(root, text="zdjęcie" , fg="Black")
+button5 = Button(bottomframe, text="wycinanie punktowe", fg="Black")
+button6 = Button(bottomframe, text="wycinanie większych obszarów", fg="Black")
+
+root.geometry("1000x700")
 
 
-
-button1.pack()
-button2.pack()
-button3.pack()
-button4.pack()
-button5.pack()
-button6.pack()
+button1.pack(fill = X)
+button2.pack(side = RIGHT )
+button3.pack(side=LEFT, fill = X)
+button4.pack(fill = BOTH)
+button5.pack( fill = X)
+button6.pack(fill = X)
 
 
 root.mainloop()
